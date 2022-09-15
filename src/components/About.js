49e3ac57b281181ec22bc8b9f3c699dc8e86d258
Marley Watson <marleykw17@gmail.com>
2022-09-15 17:27:03 -0400

    commit tree c2a3504cdbbd6d18e79a816e6ef9e92ba188586f

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { image } from "../data/data";
+console.log(image)
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+      <h2>About Me</h2>
+      <p>Hi, it's me</p>
+      <img src={image} alt="I made this"/>
+    </div>);
 }
 
 export default About;
